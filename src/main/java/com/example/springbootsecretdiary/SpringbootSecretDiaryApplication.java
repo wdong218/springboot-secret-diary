b610759f57f58902f25fe7f30740593f746fc5d5
wdong218 <wdh8890@naver.com>
2025-07-02 21:45:57 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootsecretdiary/SpringbootSecretDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecretdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecretDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecretDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsecretdiary/SpringbootSecretDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecretdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecretDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
